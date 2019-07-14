0dce359b807a2e74e0a5c8d52ef7f32df0109c53
DanyAdhi <45444857+DanyAdhi@users.noreply.github.com>
2019-07-15 01:10:15 +0700

    Update checkout.js

--- src/controllers/checkout.js
+++ src/controllers/checkout.js
@@ -68,9 +68,11 @@ exports.createCheckout = function(req, res){
 		res.status(400).send('Id address is required');
 	}else{ 
 		// const id_order = Math.random().toString(36).substring(2, 15);
-		const dateTime = getTime(); 	
-		connection.query(
-			`INSERT INTO checkout set id_order=${id_order}, id_user=${id_user}, id_product=${id_product}, total_product=${total_product}, id_address=${id_address}, total_price=${total_price}, id_payment_method=${id_payment_method}, date_checkout='${dateTime}'`,
+		const dateTime = getTime();
+		let sql = `INSERT INTO checkout set id_order='${id_order}', id_user=${id_user}, id_product=${id_product}, total_product=${total_product}, id_address=${id_address}, total_price=${total_price}, id_payment_method=${id_payment_method}, date_checkout='${dateTime}'`;
+		sql = sql.replace(/\/+/gi, '')
+		connection.query(sql
+			,
 			function(error, rows, field){
 				if(error){
 					console.log(error)
