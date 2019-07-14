38e5ba35553d7c9a2076701aa3e18d2481402fa6
DanyAdhi <45444857+DanyAdhi@users.noreply.github.com>
2019-07-15 01:01:14 +0700

    Update checkout.js

--- src/controllers/checkout.js
+++ src/controllers/checkout.js
@@ -67,16 +67,16 @@ exports.createCheckout = function(req, res){
 	}else if(!id_address){
 		res.status(400).send('Id address is required');
 	}else{ 
-		const id_order = Math.random().toString(36).substring(2, 15);
+		// const id_order = Math.random().toString(36).substring(2, 15);
 		const dateTime = getTime(); 	
 		connection.query(
-			`INSERT INTO checkout set id_order='${id_order}', id_user=${id_user}, id_product=${id_product}, total_product=${total_product}, id_address=${id_address}, total_price=${total_price}, id_payment_method=${id_payment_method}, date_checkout='${dateTime}'`,
+			`INSERT INTO checkout set id_order=${id_order}, id_user=${id_user}, id_product=${id_product}, total_product=${total_product}, id_address=${id_address}, total_price=${total_price}, id_payment_method=${id_payment_method}, date_checkout='${dateTime}'`,
 			function(error, rows, field){
 				if(error){
 					console.log(error)
 				}else{
 					return res.send({
-						data  : result,
+						data  : field,
 					})
 				}
 			}
